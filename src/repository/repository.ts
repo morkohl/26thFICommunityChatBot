984227a9a92cfcd7689493f4a4fa58e103e536f9
Moritz Kohl <Iloveproxies@protonmail.com>
2019-08-28 13:56:09 +0200

    Implement abstract repositories

---
+++ src/repository/repository.ts
@@ -0,0 +1,102 @@
+import BaseModel from "../model/model.base";
+import {Includeable} from "sequelize/types";
+import {IBasicSearchDao} from "../command/dao";
+
+export type Scope = Includeable[];
+export type NonAbstractTypeOfModel<T> = Constructor<T> & NonAbstract<typeof BaseModel>;
+type NonAbstract<T> = { [P in keyof T]: T[P] }; // "abstract" gets lost here
+type Constructor<T> = (new () => T);
+
+export class CRDRepository<Model extends BaseModel<Model>> {
+
+    constructor(protected model: NonAbstractTypeOfModel<Model>) {
+        this.model = model;
+    }
+
+    /**
+     * Searches for multiple instances in the database
+     *
+     * @param dao the data access object to search with
+     * @param scope the provided scope (defualts to #getDefaultScope)
+     * @returns the models found
+     */
+    public async search(dao: any & IBasicSearchDao, scope?: Scope): Promise<Model[]> {
+        return await this.model.findAll({
+            where: dao,
+            include: !!scope ? scope : this.getDefaultScope(),
+        });
+    }
+
+    /**
+     * Searches for one instance in the database
+     *
+     * @param dao the data access object to search with
+     * @param scope the provided scope (defaults to #getDefaultScope)
+     * @returns the model if found, else null
+     */
+    public async searchOne(dao: any & IBasicSearchDao, scope?: Scope): Promise<Model | null> {
+        return await this.model.findOne({
+            where: dao,
+            include: !!scope ? scope : this.getDefaultScope(),
+        });
+    }
+
+    /**
+     * Creates a new instance in the database
+     *
+     * @param dao the data access object to create with
+     * @param scope the provided scope (defaults to #getDefaultScope)
+     * @returns the created model
+     */
+    public async create(dao: any, scope?: Scope): Promise<Model> {
+        return await this.model.create(dao, {
+                include: scope !== undefined ? scope : this.getDefaultScope(),
+            },
+        );
+    }
+
+    /**
+     * Deletes instances in the database
+     *
+     * @param dao the data access object to search for objects to delete with
+     * @returns the number of deleted instances in the database
+     */
+    public async delete(dao?: any): Promise<number> {
+        return !!dao ? await this.model.destroy({where: dao}) : await this.model.destroy();
+    }
+
+    /**
+     * Provides the default scope
+     *
+     * @returns the default scope
+     */
+    public getDefaultScope(): Scope {
+        return [{all: true}];
+    }
+}
+
+export class CRUDRepository<Model extends BaseModel<Model>> extends CRDRepository<Model> {
+
+    /**
+     * Updates an instance in the database
+     *
+     * @param searchDao the data access object to search for update candidates
+     * @param updateDao the data access object that describes values to be updated
+     * @param scope the scope to update with
+     * @returns the updated instance if it was found
+     */
+    public async update(searchDao: any & IBasicSearchDao, updateDao: any, scope?: Scope): Promise<Model | null> {
+        const model: Model | null = await this.model.findOne({
+            where: searchDao,
+            include: !!scope ? scope : this.getDefaultScope(),
+        });
+
+        if (model === null) {
+            return model;
+        }
+
+        return await model.update(updateDao, {
+            fields: Object.keys(updateDao),
+        });
+    }
+}
